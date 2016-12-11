6d0af79b12b5c341b4efb280aeaf55a998287e82
Jack Arthur Harrhy <jack@jackharrhy.com>
2016-12-10 23:11:45 -0330

    movement system complete

--- 2048.rs
+++ 2048.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::io;
+use std::process::Command;
 use rand::Rng;
 
 type Array = [[i32; 4]; 4];
@@ -19,6 +20,19 @@ fn prompt(mut input: &mut String) {
 		.expect("Failed to read line");
 }
 
+fn get_direction_vector() -> [i32;2] {
+	let mut direction = String::new();
+	prompt(&mut direction);
+	println!("direction: {}", direction);
+	match direction.as_str() {
+		"h\n" => [-1,0],
+		"j\n" => [0,1],
+		"k\n" => [0,-1],
+		"l\n" => [1,0],
+		_ => [0,0]
+	}
+}
+
 fn random_insertion(mut grid: &mut Array) {
 	loop {
 		let random_x = rand::thread_rng().gen_range(0,4);
@@ -32,23 +46,44 @@ fn random_insertion(mut grid: &mut Array) {
 	}
 }
 
-fn get_direction_vector(mut direction_vector: &mut [i32; 2]) {
-	let mut direction = String::new();
-	prompt(&mut direction);
-	println!("direction: {}", direction);
-}
-
-fn game_loop(mut grid: &mut Array) {
-	let mut direction_vector = [0;2];
-	get_direction_vector(&mut direction_vector);
-
-	println!("{}", direction_vector[0]);
+fn clear_screen() {
+	let output = Command::new("clear").output().unwrap_or_else(|e| {
+    	panic!("failed to execute process: {}", e)
+  	});
+  	println!("{}", String::from_utf8_lossy(&output.stdout));
 }
 
 fn main() {
 	let mut grid: Array = [[0;4];4];
+	let mut prev_grid: Array;
 
-	random_insertion(&mut grid);
-	display_game(&grid);
-	game_loop(&mut grid);
+	loop {
+		random_insertion(&mut grid);
+
+		clear_screen();
+		display_game(&grid);
+
+		let direction_vector: [i32;2] = get_direction_vector();
+		println!("{},{}", direction_vector[0], direction_vector[1]);
+
+		prev_grid = [[0;4];4];
+		while grid != prev_grid {
+			prev_grid = grid;
+			for x in 0..4 {
+				for y in 0..4 {
+					let current_value: i32 = grid[x][y];
+
+					if current_value != 0 {
+						let new_x: i32 = direction_vector[0] + (x as i32);
+						let new_y: i32 = direction_vector[1] + (y as i32);
+						println!("{}, {}", new_x, new_y);
+						if new_x >= 0 && new_x <= 3 && new_y >= 0 && new_y <= 3 {
+							grid[x][y] = 0;
+							grid[new_x as usize][new_y as usize] = current_value;
+						}
+					}
+				}
+			}
+		}
+	}
 }
